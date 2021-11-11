d2f91965896c112dd0392ff2c8c18969f88f7e9e
undrown <undrown.off@gmail.com>
2021-11-11 22:11:45 +0200

    partners made

--- src/pages/Partnership/Partnership.tsx
+++ src/pages/Partnership/Partnership.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Refs from "./Refs/Refs";
 import Reward from "./Reward/Reward";
 import styles from "./Partnership.module.scss";
+const classNames = require("classnames");
 
 const Partnership = () => {
   const [activePage, setActivePage] = useState(1);
@@ -20,25 +21,29 @@ const Partnership = () => {
 
   return (
     <div className={styles.root}>
-      <div className={styles.Heading}>
-        Партнерская программа
-        </div>
-        <a
-          className={styles.ButtonBase}
-          onClick={() => {
-            setActivePage(0);
-          }}
-        >
-          Партнёрские вознаграждения
-        </a>
-        <a
-          className={styles.ButtonBase}
-          onClick={() => {
-            setActivePage(1);
-          }}
-        >
-          Рефералы
-        </a>
+      <div className={styles.Heading}>Партнерская программа</div>
+      <a
+        className={classNames({
+          [styles["ButtonBase"]]: true,
+          [styles["active"]]: activePage == 0,
+        })}
+        onClick={() => {
+          setActivePage(0);
+        }}
+      >
+        Партнёрские вознаграждения
+      </a>
+      <a
+        className={classNames({
+          [styles["ButtonBase"]]: true,
+          [styles["active"]]: activePage == 1,
+        })}
+        onClick={() => {
+          setActivePage(1);
+        }}
+      >
+        Рефералы
+      </a>
       <div className={styles.Content}>{getContent(activePage)}</div>
     </div>
   );
--- src/pages/Partnership/Refs/Refs.tsx
+++ src/pages/Partnership/Refs/Refs.tsx
@@ -1,5 +1,32 @@
+import styles from "./Refs.module.scss";
+import copyButton from './images/copyButton.svg'
+import { style } from "@mui/system";
+
 const Refs = () => {
-  return <div>Refs</div>;
+    
+    const getPartners = () => {
+        return ['partner-1', 'partner-2'];
+    }
+
+  return (
+    <div className={styles.root}>
+      <div className={styles['text-also']}>
+      Вы можете участвовать в нашей партнерской программе. Используйте личную ссылку ниже.
+      </div>
+      <div className={styles['text-secondary']}>
+      Ваша партнерская ссылка
+      </div>
+      <input className={styles['ref-link']} readOnly={true} value={'https://www.cryptomercgant.com/ref/gmwefrgolrr69'}/>
+      <img className={styles['copy-button']} src={copyButton}/>
+      <div className={styles.partners}>
+          <div className={styles['partners-title']}>Мои партнёры</div>
+          {getPartners().map((i) => {
+              return <div className={styles['partner-item']}>{i}</div>
+          })}
+          <div className={styles['load-more-button']}>Load more</div>
+      </div>
+    </div>
+  );
 };
 
 export default Refs;
